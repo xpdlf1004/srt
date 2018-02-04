2868dbf281371922e45a03d57266e74bc7558ac2
Gilyoung Kim <xpdlf1004@gmail.com>
2018-02-05 02:00:20 +0900

    Update.

--- app.js
+++ app.js
@@ -24,29 +24,31 @@ app.use(session({
 
 axios.defaults.baseURL = 'https://etk.srail.co.kr';
 
-app.post('/login', function (req, res) {
+app.post('/login', function(req, res) {
   axios.post('/cmc/01/selectLoginInfo.do?pageId=TK0701000000', qs.stringify({
     srchDvNm: req.body.userNumber,
     hmpgPwdCphd: req.body.userPassword,
     srchDvCd: '1'
-  })).then(function (response) {
+  })).then(function(response) {
+    console.log(response.headers);
     if (response.data && response.data.includes('location.replace(\'/main.do\');')) {
       // 로그인 성공
       // 쿠키값 세션에 저장
       if (response.headers && response.headers['set-cookie'] && response.headers['set-cookie'].length > 0) {
-        sessionKey = response.headers['set-cookie'][0].split(';')[0].split('=')[1];
+        sessionKey = response.headers['set-cookie'][0].split(';')[0].split('JSESSIONID_ETK=')[1];
         req.session['JSESSIONID_ETK'] = sessionKey;
+        console.log(sessionKey);
       }
       res.status(200).send('success');
     } else {
       res.status(500).send('invalid id or password');
     }
-  }).catch(function (error) {
+  }).catch(function(error) {
     res.status(500).send('server error');
   });
 });
 
-app.get('/trainList', function (req, res) {
+app.get('/trainList', function(req, res) {
   var startStation = req.query.startStation;
   var startStationNumber = req.query.startStationNumber;
   var endStation = req.query.endStation;
@@ -73,7 +75,7 @@ app.get('/trainList', function (req, res) {
     locSeatAttCd1: '000',
     rqSeatAttCd1: '015',
     trnGpCd: '300'
-  })).then(function (response) {
+  })).then(function(response) {
     var $ = cheerio.load(response.data);
     trainList = [];
     $('#result-form table tbody tr').each(function(trIndex, trElem) {
@@ -95,7 +97,7 @@ app.get('/trainList', function (req, res) {
       }
     });
     res.status(200).send(trainList);
-  }).catch(function (error) {
+  }).catch(function(error) {
     res.status(500).send('server error');
   });
 });
@@ -118,9 +120,90 @@ app.post('/logout', function(req, res) {
   })
 })
 
+app.post('/reserveTrain', function(req, res) {
+  axios.post('/hpg/hra/01/checkUserInfo.do?pageId=TK0101010000', qs.stringify({
+    rsvTpCd: '01',
+    jobId: '1101',
+    jrnyTpCd: '11',
+    jrnyCnt: '1',
+    totPrnb: '1',
+    stndFlg: 'N',
+    trnOrdrNo1: '1', // 기차 순번
+    jrnySqno1: '001',
+    runDt1: '20180205', 
+    trnNo1: '00303', // 기차번호
+    trnGpCd1: '300',
+    stlbTrnClsfCd1: '17',
+    dptDt1: '20180205',// 출발 날짜
+    dptTm1: '060000',// 출발 시간
+    dptRsStnCd1: '0551', // 출발역
+    dptStnConsOrdr1: '000001',// 출발역
+    dptStnRunOrdr1: '000001',// 출발역
+    arvRsStnCd1: '0020',// 도착역
+    arvStnConsOrdr1: '000020',// 도착역
+    arvStnRunOrdr1: '000006',// 도착역
+    scarYn1: 'N',
+    scarGridcnt1: '',
+    scarNo1: '',
+    seatNo1_1: '',
+    seatNo1_2: '',
+    seatNo1_3: '',
+    seatNo1_4: '',
+    seatNo1_5: '',
+    seatNo1_6: '',
+    seatNo1_7: '',
+    seatNo1_8: '',
+    seatNo1_9: '',
+    psrmClCd1: '1',
+    smkSeatAttCd1: '000',
+    dirSeatAttCd1: '000',
+    locSeatAttCd1: '000',
+    rqSeatAttCd1: '015',
+    etcSeatAttCd1: '000',
+    psgGridcnt: '1',
+    psgTpCd1: '1',
+    psgInfoPerPrnb1: '1',
+    psgTpCd2: '',
+    psgInfoPerPrnb2: '',
+    psgTpCd3: '',
+    psgInfoPerPrnb3: '',
+    psgTpCd4: '',
+    psgInfoPerPrnb4: '',
+    psgTpCd5: '',
+    psgInfoPerPrnb5: '',
+    mutMrkVrfCd: '',
+    reqTime: (new Date()).getTime(),
+    crossYn: 'N',
+  }), {
+    headers: {
+      Cookie: 'JSESSIONID_ETK=' + req.session.JSESSIONID_ETK
+    }
+  }).then(function(response) {
+    // location.replace('/cmc/01/selectLoginForm.do?pageId=TK0701000000&rsvTpCd=07btYNcdmWD9MWHh3EKBQA%3D%3D');
+    // 로그인 다시 시키기
+    console.log(response.data);
+
+    // else replace requestReservationInfo.do 이면
+    axios.get('/hpg/hra/02/requestReservationInfo.do?pageId=TK0101030000', {
+      headers: {
+        Cookie: 'JSESSIONID_ETK=' + req.session.JSESSIONID_ETK
+      }
+    })
+    .then(function(response) {
+      // /hpg/hra/02/selectReservationList.do?pageId=TK0102010000 (예약 대기 페이지)
+      console.log(response);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }).catch(function(error) {
+    console.log(error);
+    res.status(500).send('server error');
+  })
+});
+
 app.get('/stationList', function(req, res) {
   axios.post('/hpg/hra/01/selectMapInfo.do?isAll=Y&other=&target=dpt&pageId=TK0101010000')
-  .then(function (response) {
+  .then(function(response) {
     var $ = cheerio.load(response.data);
     var stationList = [];
     $('#wrap .map>ul>li').each(function(index, elem) {
@@ -139,7 +222,7 @@ app.get('/stationList', function(req, res) {
     });
     res.status(200).send(stationList);
   })
-  .catch(function (error) {
+  .catch(function(error) {
     res.status(500).send('server error');
   });
 })
